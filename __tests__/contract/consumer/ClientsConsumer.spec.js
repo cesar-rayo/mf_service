7851247d436de48df5bec1cacefd271044dc1e04
Cesar Augusto Rayo Zapata <cesar-rayo@hotmail.es>
2021-03-08 10:47:34 -0500

    first commit

---
+++ __tests__/contract/consumer/ClientsConsumer.spec.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const { Matchers } = require("@pact-foundation/pact")
+
+
+describe("Clients Service", () => {
+    const GET_EXPECTED_BODY = [{
+        "fname": "Kent",
+        "id": 2,
+        "lname": "Brockman"
+      },
+      {
+        "fname": "Bunny",
+        "id": 3,
+        "lname": "Easter"
+      },
+      {
+        "fname": "Doug",
+        "id": 1,
+        "lname": "Farrell"
+    }]
+    
+
+
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/people")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr key={item.id}>
+                <td>{item.fname}</td>
+                <td>{item.lname}</td>
+                <td>{item.timestamp}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  }
 }
 
-export default App;
+export default MyComponent;
